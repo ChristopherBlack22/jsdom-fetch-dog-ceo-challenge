90862a053a6d1c06d09c5379d2abb6af6b565f15
Christopher <christopher.black.2209@googlemail.com>
2021-11-19 16:46:30 +0000

    commit tree dab815e894bf7982f0df846a6c6091335f866deb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+//function dogImageUrls {
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => console.log(json.message))
+//}
+
+// document.createElement("img");
+
+// create 4 new image elements, set their src to the json array values, find id="dog-image-container" and append them
+// const imgContainer = document.getElementByID("dog-image-container")
+
+// for (let i = 0; i < message.length; i++) {
+//     let image = document.createElement("img");
+//     image.src = 
+//     imgContainer.appendChild(image)
+// }
